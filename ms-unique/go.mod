82cb5a89a72455247a4dd401de8ccc61a552d3e6
ssung <sungsharon@gmail.com>
2025-08-30 17:35:35 -0400

    fix: adjusted the makefile for first test and prepended changelog

--- ms-unique/go.mod
+++ ms-unique/go.mod
@@ -1,3 +1,8 @@
 module ms-unique
 
 go 1.24.6
+
+require (
+	github.com/google/uuid v1.6.0 // indirect
+	github.com/jepsen-io/maelstrom/demo/go v0.0.0-20250806145204-447d18a7c07e // indirect
+)
---
+++ ms-unique/main.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"log"
+
+	"github.com/google/uuid"
+	maelstrom "github.com/jepsen-io/maelstrom/demo/go"
+)
+
+func main() {
+	// logger := slog.New(slog.NewJSONHandler(os.Stdout, nil))
+	// logger.Info("Shoop")
+	n := maelstrom.NewNode()
+
+	n.Handle("generate", func(msg maelstrom.Message) error {
+		result := map[string]any{}
+		id := uuid.New()
+
+		result["type"] = "generate_ok"
+		result["id"] = id.String()
+
+		return n.Reply(msg, result)
+	})
+
+	err := n.Run()
+
+	if err != nil {
+		log.Fatal(err)
+	}
+}
